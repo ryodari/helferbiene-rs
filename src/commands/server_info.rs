bf74c916e646e6aaefce1d7dfa6501f511632ffb
ryodari <franz-josef@gobry.eu>
2024-02-22 03:45:22 +0100

    finish basic serverinfo command

--- src/commands/server_info.rs
+++ src/commands/server_info.rs
@@ -1,13 +1,19 @@
+use image::EncodableLayout;
+
+use itertools::Itertools;
 use serenity::{
     all::{
-        CommandInteraction, CommandOptionType, Context, CreateCommand, CreateCommandOption,
-        CreateEmbed, CreateInteractionResponse, CreateInteractionResponseMessage,
-        EditInteractionResponse, ResolvedOption, ResolvedValue,
+        Colour, CommandInteraction, CommandOptionType, Context, CreateAttachment, CreateCommand,
+        CreateCommandOption, CreateEmbed, CreateEmbedFooter, CreateInteractionResponse,
+        CreateInteractionResponseMessage, EditInteractionResponse, ResolvedOption, ResolvedValue,
     },
     async_trait,
 };
 
-use crate::minecraft;
+use crate::minecraft::{
+    self,
+    packet::slp::{SlpResponse, SlpServerDescription},
+};
 
 use super::Command;
 
@@ -49,11 +55,19 @@ impl Command for ServerInfoCommand {
     ) -> serenity::Result<Option<CreateInteractionResponse>> {
         let options = command.data.options();
 
-        let host = match options.first() {
+        let (host, port) = match options.first() {
             Some(ResolvedOption {
                 value: ResolvedValue::String(host),
                 ..
-            }) => host,
+            }) => {
+                let split_host: Vec<&str> = host.split(":").collect();
+
+                if split_host.len() >= 1 {
+                    (split_host[0], split_host[0].parse::<u16>().ok())
+                } else {
+                    (*host, None)
+                }
+            }
             _ => {
                 return Ok(Some(CreateInteractionResponse::Message(
                     CreateInteractionResponseMessage::new().content("Please provide a host"),
@@ -61,32 +75,149 @@ impl Command for ServerInfoCommand {
             }
         };
 
-        let port: u16 = match options.iter().find(|o| o.name == "port") {
-            Some(ResolvedOption {
-                value: ResolvedValue::Integer(port),
-                ..
-            }) => *port as u16,
-            _ => 25565,
+        let port = match port {
+            Some(port) => port,
+            None => match options.iter().find(|o| o.name == "port") {
+                Some(ResolvedOption {
+                    value: ResolvedValue::Integer(port),
+                    ..
+                }) => *port as u16,
+                _ => 25565,
+            },
         };
 
         // defer
         command.defer(&ctx.http).await?;
 
         let client = minecraft::client::Client::new(host.to_string(), port).await?;
-        let mut info = client.status().await?;
+        let info: SlpResponse = client.status().await?;
 
-        info.favicon = Some("ignored".into());
+        let description = match info.description {
+            SlpServerDescription::Simple(ref description) => description.clone(),
+            SlpServerDescription::Complex(ref component) => component.format_string(),
+        };
 
-        let pretty = serde_json::to_string_pretty(&info).unwrap();
+        let favicon = info.favicon;
 
-        let embed = CreateEmbed::new()
+        let mut embed = CreateEmbed::new()
             .title(format!("{}:{}", host, port))
-            .description(format!("```json\n{}```", pretty));
+            .field("MOTD", description, false)
+            .field(
+                "Players",
+                format!("{}/{}", info.players.online, info.players.max),
+                false,
+            )
+            .footer(CreateEmbedFooter::new("helferbiene-rs"));
 
-        let data = EditInteractionResponse::new().add_embed(embed);
+        if let Some(sample) = info.players.sample {
+            if !sample.is_empty() {
+                let all_players = sample.len() == info.players.online as usize;
 
-        command.edit_response(&ctx.http, data).await?;
+                let mut formatted = sample
+                    .iter()
+                    .map(|p| format!("[{}](https://namemc.com/search?q={})", p.name, p.id))
+                    .join("\n");
+
+                if !all_players {
+                    formatted.push_str("\n...");
+                }
+
+                embed = embed.field("", &formatted, false);
+            }
+        }
+
+        embed = embed.field("Version", info.version.name, true).field(
+            "Protocol",
+            info.version.protocol.to_string(),
+            true,
+        );
+
+        if let Some(mod_info) = info.modinfo {
+            if mod_info.type_ == "FML" && !mod_info.mod_list.is_empty() {
+                let formatted = mod_info
+                    .mod_list
+                    .iter()
+                    .map(|m| format!("{}@{}", m.modid, m.version))
+                    .join("\n");
+
+                let chunks = split_into_chunks(formatted.as_str(), 1024);
+                for (index, chunk) in chunks.iter().enumerate() {
+                    let field_name = match index {
+                        0 => "Mods",
+                        _ => "",
+                    };
+                    embed = embed.field(field_name, chunk, false);
+                }
+            }
+        }
+
+        let mut response = EditInteractionResponse::new();
+
+        if let Some(favicon) = favicon {
+            let favicon = match favicon.strip_prefix("data:image/png;base64,") {
+                Some(f) => f.to_string(),
+                None => favicon,
+            };
+
+            if let Ok(decoded) = {
+                use base64::Engine;
+                base64::prelude::BASE64_STANDARD.decode(favicon)
+            } {
+                if let Ok(image) = image::load_from_memory(&decoded) {
+                    match color_thief::get_palette(
+                        image.to_rgba8().as_bytes(),
+                        color_thief::ColorFormat::Rgba,
+                        10,
+                        2,
+                    ) {
+                        Ok(colors) => {
+                            if let Some(color) = colors.first() {
+                                embed = embed.colour(Colour::from_rgb(color.r, color.g, color.b));
+                            }
+                        }
+                        Err(e) => log::error!("Failed to extract dominant color: {}", e),
+                    };
+                }
+
+                let attachment = CreateAttachment::bytes(decoded, "favicon.png");
+                response = response.new_attachment(attachment);
+                embed = embed.thumbnail("attachment://favicon.png");
+            }
+        }
+
+        response = response.add_embed(embed);
+
+        command.edit_response(&ctx.http, response).await?;
 
         return Ok(None);
     }
+}
+
+fn split_into_chunks(input: &str, max_chunk_size: usize) -> Vec<String> {
+    let mut result = Vec::new();
+    let mut current_chunk = String::new();
+    let mut current_size = 0;
+
+    for line in input.lines() {
+        // Check if adding the line exceeds the max_chunk_size
+        if current_size + line.len() > max_chunk_size {
+            // If it exceeds, push the current chunk into the result
+            result.push(current_chunk.clone());
+            // Reset current_chunk and current_size
+            current_chunk.clear();
+            current_size = 0;
+        }
+
+        // Add the line to the current_chunk
+        current_chunk.push_str(line);
+        current_chunk.push('\n'); // Add back the newline character
+        current_size += line.len() + 1; // Add the length of the line plus 1 for the newline character
+    }
+
+    // Push the last chunk
+    if !current_chunk.is_empty() {
+        result.push(current_chunk);
+    }
+
+    result
 }
--- src/minecraft/text_component.rs
+++ src/minecraft/text_component.rs
@@ -1,6 +1,8 @@
 use serde::{Deserialize, Serialize};
 use serde_with::skip_serializing_none;
 
+use super::packet::slp::SlpServerDescription;
+
 #[skip_serializing_none]
 #[derive(Deserialize, Serialize, Debug)]
 #[serde(rename_all = "camelCase")]
@@ -40,7 +42,7 @@ pub struct TextComponentClick {
 pub struct TextComponent {
     #[serde(rename = "type")]
     pub type_: Option<TextComponentType>,
-    pub extra: Option<Vec<Self>>,
+    pub extra: Option<Vec<SlpServerDescription>>,
 
     // Type: text
     pub text: String,
@@ -55,4 +57,57 @@ pub struct TextComponent {
     pub font: Option<String>, // only valid on 1.16+
     pub insertion: Option<String>, // to insert text when clicked in chat
                               // pub click_event: Option<TextComponentClick>,
+}
+
+impl TextComponent {
+    pub fn format_string(&self) -> String {
+        match self.type_ {
+            Some(TextComponentType::Text) => {}
+            None => {}
+            _ => return "".into(),
+        };
+
+        let mut res = self.text.clone();
+
+        let mut is_wrapped = false;
+        let mut wrap_str = |chars| {
+            res.insert_str(0, chars);
+            res.push_str(chars);
+            is_wrapped = true;
+        };
+
+        let is_true = |arg: &Option<bool>| arg.is_some_and(|x| x == true);
+
+        if is_true(&self.bold) && is_true(&self.italic) {
+            wrap_str("***");
+        } else if is_true(&self.bold) {
+            wrap_str("**");
+        } else if is_true(&self.italic) {
+            wrap_str("*");
+        }
+
+        if is_true(&self.underlined) {
+            wrap_str("__");
+        }
+
+        if is_true(&self.strikethrough) {
+            wrap_str("~~");
+        }
+
+        if is_wrapped {
+            res.push('\u{200B}'); // zero width to seperate markdown
+        }
+
+        if let Some(childs) = &self.extra {
+            for child in childs {
+                let child_string = match child {
+                    SlpServerDescription::Simple(x) => x.clone(),
+                    SlpServerDescription::Complex(x) => x.format_string(),
+                };
+                res.push_str(child_string.as_str());
+            }
+        }
+
+        res
+    }
 }
